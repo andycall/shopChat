d1e020017970c65bc5cf11198713c599099aca55
huangziyi01 <huangziyi01@baidu.com>
2015-12-12 16:41:47 +0800

    update

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+var webpack = require('webpack')
+var path = require('path')
+
+module.exports = {
+    entry: {
+        app: [
+            './src/index.js'
+        ]
+    },
+
+    output: {
+        path: path.join(__dirname, 'output'),
+        filename: 'bundle.js',
+        publicPath: '/output/'
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.js?$/,
+                exclude: /node_modules/,
+                loaders: ['babel?presets[]=react,presets[]=es2015', 'html-path-loader']
+            }, {
+                test: /\.(scss|css)/,
+                exclude: /node_modules/,
+                loaders: ['style', 'css', 'autoprefixer', 'sass', 'css-path-loader']
+            }, {
+                test: /\.(scss|css)/,
+                include: /node_modules/,
+                loaders: ['style', 'css', 'autoprefixer', 'sass']
+            }, {
+                test: /\.(png|jpg)$/,
+                exclude: /node_modules/,
+                loader: 'url?limit=3000&name=img/[hash:8].[name].[ext]'
+            }, {
+                test: /\.(woff|woff2|ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'url'
+            }, {
+                test: /\.json$/,
+                loader: 'json-loader'
+            }
+        ]
+    }
+}
